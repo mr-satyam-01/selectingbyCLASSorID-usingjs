7476fb75dd5d392e9e68b3c56ee8cc93fc32b2a0
satyam kumar <satyamsingh4257@gmail.com>
2025-02-08 14:52:57 +0530

    Add files via upload
    some dom manipulation practice

---
+++ Exercise12.js
@@ -0,0 +1,20 @@
+//Q. select the boxes by class and change the background color
+
+let boxes =  document.getElementsByClassName("box1");
+
+boxes[1].style.backgroundColor = "orange";
+boxes[3].style.backgroundColor = "pink";
+boxes[4].style.backgroundColor = "lightgreen";
+boxes[0].style.backgroundColor = "lightblue";
+
+
+let boxed1 = document.getElementById("select1");
+let boxed2 = document.getElementById("select2");
+let boxed3 = document.getElementById("select3");
+let boxed4 = document.getElementById("select4");
+
+
+boxed1.style.color = "blue";
+boxed2.style.color = "brown";
+boxed3.style.color = "red";
+boxed4.style.color = "purple";
